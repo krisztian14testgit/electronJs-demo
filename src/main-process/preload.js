96ea3aad2e163d4b68bef361089d2dd8a5112a81
krisztian14testgit <krisztian14@citromail.hu>
2022-08-23 11:43:17 +0200

    contextBridge brief docs asdded

--- src/main-process/preload.js
+++ src/main-process/preload.js
@@ -1,7 +1,10 @@
 "use strict";
 
 /** 
- * contextBrige: 
+ * contextBrige: module can be used to safely expose APIs from your preload script's isolated context 
+ * 			to the context the website is running in. 
+ *			The API will also be accessible from the website on window.myAPI just like it was before.
+ * link: https://www.electronjs.org/docs/latest/tutorial/context-isolation
 */
 const { contextBridge, ipcRenderer } = require('electron');
 // You CANNOT IMPORT nodejs library here, as you CANNOT USE IT under electron!!!
